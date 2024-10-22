31400f4013a13257488af1a45bc8ff11ddd3c834
Vsakoe <140807059+Vsakoe@users.noreply.github.com>
2024-10-23 00:56:41 +0300

    Update doxer.py

--- doxer.py
+++ doxer.py
@@ -1,9 +1,7 @@
 __version__ = (1, 0, 2)
 
 #            © Copyright 2024
-
 #           https://t.me/HikkTutor 
-#
 # 🔒      Licensed under the GNU AGPLv3
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
 #┏┓━┏┓━━┏┓━━┏┓━━┏━━━━┓━━━━━┏┓━━━━━━━━━━━━
